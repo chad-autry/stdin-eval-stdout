0a1761e3d95ecefee6952c031743b21c86723578
Chad Autry <chad.autry@gmail.com>
2015-06-21 21:18:21 -0500

    I suspect spawn-cmd works for windows, but breaks TravisCI

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 //I hear you like to run node in your node, so I'm testing node using node
-var spawn = require('spawn-cmd').spawn,
+var spawn = require('child_process').spawn,
     testProcess = spawn('node ./src/index.js < ./tests/eval.js'),
     assert = require('assert');
 console.log("Modules required and process spawned");
